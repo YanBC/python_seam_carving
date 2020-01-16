c723f36967c05d61e7d89096235bccb0393f30da
yanbc <imyanbc@gmail.com>
2020-01-16 23:21:33 +0800

    add flask web application

--- cml_ui.py
+++ cml_ui.py
@@ -1,25 +1,58 @@
 import numpy as np
 import argparse
-import cv2 as cv
+from cv2 import resize as imresize
+from imageio import imread, imwrite
 
-from carver import carve_column
+
+try:
+    from carver.carve_fast import carve_column
+except ModuleNotFoundError:
+    from carver.carve_slow import carve_column
+    print('#@@@@@@@@@@@@@@@@@@@#')
+    print('Using slow version seam carving')
+    print('#@@@@@@@@@@@@@@@@@@@#')
 
 
-def crop_c(img, scale_c):
-    r, c, _ = img.shape
-    new_c = int(scale_c * c)
 
-    for i in range(c - new_c):
-        img = carve_column(img)
 
-    return img
+class Engine():
+    def __init__(self):
+        self.MAX_WIDTH = 5000
+        self.MAX_HEIGHT = 5000
 
-def crop_r(img, scale_r):
-    img = np.rot90(img, 1, (0, 1))
-    img = crop_c(img, scale_r)
-    img = np.rot90(img, 3, (0, 1))
-    return img
+    def _crop_c(self, img, num):
+        r, c, _ = img.shape
 
+        for i in range(num):
+            img = carve_column(img)
+
+        return img
+
+    def _crop_r(self, img, num):
+        img = np.rot90(img, 1, (0, 1))
+        img = self._crop_c(img, num)
+        img = np.rot90(img, 3, (0, 1))
+        return img
+
+
+    def run(self, img, target_width, target_height):
+        if target_width > self.MAX_WIDTH or target_height > self.MAX_HEIGHT:
+            print(f'target height and width must be less than {self.MAX_HEIGHT} and {self.MAX_WIDTH} respectively')
+            return None
+
+        img_h, img_w, _ = img.shape
+        scale_h = target_height / img_h
+        scale_w = target_width / img_w
+        scale = max(scale_h, scale_w)
+
+        img_scaled = imresize(img, (0,0), fx=scale, fy=scale)
+        rows_tobe_carved = img_scaled.shape[0] - target_height
+        cols_tobe_carved = img_scaled.shape[1] - target_width
+
+        out = self._crop_r(img_scaled, rows_tobe_carved)
+        out = self._crop_c(out, cols_tobe_carved)
+
+        return out
 
 
 
@@ -27,28 +60,32 @@ def crop_r(img, scale_r):
 
 def opts_parser():
     p = argparse.ArgumentParser()
-    p.add_argument('axis', choices=['r', 'c'], help='resize in rows(r) or columns(c)')
-    p.add_argument('scale', type=float, help='resize scale')
     p.add_argument('src', help='path to source image')
+    p.add_argument('width', type=int, help='target width to be converted into')
+    p.add_argument('height', type=int, help='target height to be converted into')
     p.add_argument('--des', default='res.jpg', help='where to store the resized image')
     return p.parse_args()
+
+
 
 def main():
     opts = opts_parser()
 
-    which_axis = opts.axis
-    scale = opts.scale
+    target_width = opts.width
+    target_height = opts.height
     in_filename = opts.src
     out_filename = opts.des
 
-    img = cv.imread(in_filename)
+    carving_engine = Engine()
 
-    if which_axis == 'r':
-        out = crop_r(img, scale)
-    elif which_axis == 'c':
-        out = crop_c(img, scale)
+    img = imread(in_filename)
+
+    out = carving_engine.run(img, target_width, target_height)
     
-    cv.imwrite(out_filename, out)
+    if out is not None:
+        imwrite(out_filename, out)
+    else:
+        return
 
 
 if __name__ == '__main__':
---
+++ web_ui.py
@@ -0,0 +1,77 @@
+from flask import Flask, request, render_template
+from flask import redirect, url_for, flash
+from flask import send_from_directory
+import os
+from werkzeug.utils import secure_filename
+from imageio import imread, imwrite
+
+from cml_ui import Engine
+
+
+
+
+UPLOAD_FOLDER = './uploads'
+IMAGE_FOLDER = './results'
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['IMAGE_FOLDER'] = IMAGE_FOLDER
+
+carver = Engine()
+
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+
+        filename = secure_filename(file.filename)
+        srcPath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(srcPath)
+
+        target_width = int(request.form['width'])
+        target_height = int(request.form['height'])
+        image = imread(srcPath)
+
+        output = carver.run(image, target_width, target_height)
+
+        if output is not None:
+            desPath = os.path.join(app.config['IMAGE_FOLDER'], filename)
+            imwrite(desPath, output)
+            return redirect(url_for('show_file', filename=filename))
+        else:
+            return "Sorry, something went wrong"
+
+
+    return render_template('upload.html')
+
+
+
+
+@app.route('/images/<filename>')
+def show_file(filename):
+    return send_from_directory(app.config['IMAGE_FOLDER'], filename)
+
+
+
+
+if __name__ == '__main__':
+    if not os.path.isdir(app.config['UPLOAD_FOLDER']):
+        os.mkdir(app.config['UPLOAD_FOLDER'])
+    if not os.path.isdir(app.config['IMAGE_FOLDER']):
+        os.mkdir(app.config['IMAGE_FOLDER'])
+
+    app.config['ENV'] = 'development'
+    app.config['MAX_CONTENT_LENGTH'] = 124 * 1024 * 1024
+    app.run(host='', port=13456)
+
